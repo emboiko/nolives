45d44977bc374c18e2d3415b1bbffb8589ea08f3
Emboiko <emboiko@gmail.com>
2021-01-10 00:15:17 -0500

    userRouter & Landing structuring

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -3,29 +3,91 @@ import React, { Component } from 'react';
 import '../App.css';
 
 export default class Landing extends Component {
+  constructor() {
+    super();
+    this.state = { loaded: false, profile: null };
+  }
+
   componentDidMount = async () => {
     if (this.props.user) {
       const res = await fetch("/user/profile");
       const data = await res.json();
-      console.log(data);
+      this.setState({ profile: data });
     }
+    this.setState({ loaded: true });
+  }
+
+  handleClick = async (e) => {
+    const res = await fetch(e.target.dataset.href, {
+      method: "POST"
+    });
+    console.log(await res.json());
   }
 
   render() {
-    if (this.props.user) {
-      return (
-        <>
-          <p>{this.props.user.battletag}</p>
-        </>
-      );
+    if (this.state.loaded) {
+      if (this.props.user) {
+        console.log(this.state.profile);
+
+        let characters = [];
+
+        this.state.profile.wow_accounts.forEach((account) => {
+          account.characters.forEach((character) => {
+
+            characters.push({
+              name: character.name,
+              id: character.id,
+              realm: {
+                name: character.realm.name,
+                id: character.realm.id,
+                slug: character.realm.slug,
+              }
+            });
+
+          });
+        })
+
+        const jsxCharacters = characters.map((character, i) => {
+          return (
+            <div key={i}>
+              <p>{character.name}</p>
+              <button
+                data-href={
+                  `/user/register?realmId=${character.realm.id}` +
+                  `&characterId=${character.id}` +
+                  `&realmSlug=${character.realm.name}` +
+                  `&characterName=${character.name}`
+                }
+                onClick={this.handleClick}
+              >
+                Register
+              </button>
+            </div>
+          );
+        });
+
+        return (
+          <>
+            <h3>Characters:</h3>
+            {jsxCharacters}
+          </>
+        );
+
+      } else {
+        return (
+          <>
+            <p>Landing</p>
+          </>
+        );
+
+      }
 
     } else {
       return (
         <>
-          <p>No user in landing</p>
+          <p>Loading...</p>
         </>
       );
-
     }
 
   }
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -19,4 +19,27 @@ userRouter.get("/user/profile", auth, async (req, res) => {
   res.json(data);
 });
 
+userRouter.post("/user/register", auth, async (req, res) => {
+  const region = req.query.region || "us";
+
+  const headers = {
+    Authorization: `Bearer ${req.user.token}`,
+  };
+
+  let url = `https://${region}.api.blizzard.com/profile/user/wow/protected-character/`;
+  url += `${req.query.realmId}-${req.query.characterId}`;
+  url += `?:region=${region}&namespace=profile-us&locale=en_US`;
+
+
+  const response = await fetch(url, { headers });
+  const data = await response.json();
+
+  const deaths = data.protected_stats.total_number_deaths;
+  if (deaths !== 0) return res.json({ "error": "Total deaths !== 0" });
+
+
+
+  res.json({ "placeholder": "placeholder" });
+});
+
 module.exports = userRouter;
